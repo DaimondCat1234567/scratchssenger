26fa66005564d4634ac9a418e442b2075730505b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 15:49:10 +0300

    Update data.js

--- api/help/data.js
+++ api/help/data.js
@@ -1,7 +1,9 @@
 const project = "scratchssenger"
 const root = "/"
+const repository = "https://github.com/daimondcat1234567/scratchssenger/"
 
 export {
   project,
-  root
+  root,
+  repository
 }
