083a6b86ee0aaf7793557fef73eff4740e559a72
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 15:13:29 +0300

    Update index.js

--- desktop/index.js
+++ desktop/index.js
@@ -1,3 +1,16 @@
 import electron from "electron"
 import path from "node:path"
 const { app, BrowserWindow } = electron
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
