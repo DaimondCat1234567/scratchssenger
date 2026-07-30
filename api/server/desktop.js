450d023d88f920329a66277f6094cf4f8eb37503
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 16:06:49 +0300

    Update desktop.js

--- api/server/desktop.js
+++ api/server/desktop.js
@@ -4,6 +4,9 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  const { path } = req.query
+
   res.status(200).json({
     ok: true
   })
