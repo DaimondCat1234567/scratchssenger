50e47460ca1cb52b0d530628038985fc0f68a480
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 15:19:39 +0300

    Update index.js

--- desktop/index.js
+++ desktop/index.js
@@ -5,12 +5,27 @@ const { app, BrowserWindow } = electron
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
-  win.loadFile('index.html')
+  win.loadFile('../msngrv144.html')
 }
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
