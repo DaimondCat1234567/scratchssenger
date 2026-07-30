586d9a818fd4312980c1df81f59b6580cec0d5c6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 14:21:32 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
-
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.status(200).json({
+    ok: true
+  })
+}
