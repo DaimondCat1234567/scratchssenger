f77e3d9ef0f001e6d200033b8ae8a207ad307310
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 15:50:28 +0300

    Update desktop.js

--- api/server/desktop.js
+++ api/server/desktop.js
@@ -1,1 +1,10 @@
+import { repository } from "../help/data.js"
 
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.status(200).json({
+    ok: true
+  })
+}
