9f897b13faa580828c418d291100a5ccfacf5b56
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-30 15:53:24 +0300

    Update data.js

--- api/help/data.js
+++ api/help/data.js
@@ -1,9 +1,11 @@
 const project = "scratchssenger"
 const root = "/"
 const repository = "https://github.com/daimondcat1234567/scratchssenger/"
+const releaseStart = `${repository}/archive/refs/tags/`
 
 export {
   project,
   root,
-  repository
+  repository,
+  releaseStart
 }
