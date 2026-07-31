11dcbc65ff2e503001266c49dc035cdbb5b9a1ff
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-31 10:03:07 +0300

    Update db.js

--- api/apis/db.js
+++ api/apis/db.js
@@ -1,2 +1,4 @@
 import { DB_URL, DB_SECRET } from "../help/env.js"
-import { project, root } from "../help/data.js"
+import { project, root, pathChats, pathUsers } from "../help/data.js"
+
+const connect = () => {}
