96b0d02840409971b7be931c590c2537e871582e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-07-31 10:20:07 +0300

    Upd

--- api/apis/db.js
+++ api/apis/db.js
@@ -1,4 +1,18 @@
 import { DB_URL, DB_SECRET } from "../help/env.js"
 import { project, root, pathChats, pathUsers } from "../help/data.js"
 
-const connect = () => {}
+const connect = async () => {
+  const connecT = await (await fetch(`https://${DB_URL}/connect`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringtify({
+      DB_SECRET,
+      group: "DCG"
+    })
+  })).json()
+  return connecT
+}
+
+export { connect }
