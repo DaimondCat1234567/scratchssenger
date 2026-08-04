7066f1b206bdf2d864f0d26eac154cb4852612bc
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-04 16:35:07 +0300

    Create sobuild.js

---
+++ scripts/sobuild.js
@@ -0,0 +1,11 @@
+import generate from "../sodesktop/build.js"
+import * as readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const type = await rl.question('Type: ');
+const version = await rl.question('Version: ');
+const versionFile = version.replaceAll(".", "")
+
+rl.close()
