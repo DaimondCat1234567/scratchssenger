1fc44cfcfa33fa5c25fd81b68a8809866a873414
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-04 18:34:00 +0300

    Update data.js

--- api/help/data.js
+++ api/help/data.js
@@ -4,8 +4,8 @@ const repository = "https://github.com/daimondcat1234567/scratchssenger/"
 const releaseStart = `${repository}archive/refs/tags/`
 const pathChats = `${root}chats`
 const pathUsers = `${root}users`
-const rawRepo = ""
-const sobuildStart = ``
+const rawRepo = "https://raw.githubusercontent.com/DaimondCat1234567/scratchssenger/refs/heads/main/"
+const sobuildStart = `${ramRepo}sodesktop/builds/`
 
 export {
   project,
