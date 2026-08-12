ad91c00a68161e353d9b30aad84c161cc3e60caf
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-12 21:45:57 +0300

    Update db.js

--- api/apis/db.js
+++ api/apis/db.js
@@ -17,4 +17,39 @@ const connect = async () => {
   return connecT
 }
 
-export { connect }
+const read = async (path) => {
+  const connecT = await (await fetch(`https://${DB_URL}/files/read/`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "bypass-tunnel-reminder": "true"
+    },
+    body: JSON.stringtify({
+      DB_SECRET,
+      projectGroup,
+      project,
+      path
+    })
+  })).json()
+  return connecT.result
+}
+
+const write = async (path, newContent) => {
+  const connecT = await (await fetch(`https://${DB_URL}/files/write/`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "bypass-tunnel-reminder": "true"
+    },
+    body: JSON.stringtify({
+      DB_SECRET,
+      projectGroup,
+      project,
+      path,
+      newContent
+    })
+  })).json()
+  return connecT.result
+}
+
+export { connect, read, write }
