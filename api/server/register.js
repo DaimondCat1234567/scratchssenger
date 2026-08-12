2ccb22e9a734976aaf83f4bd88d9a95f34d2e2fd
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-12 22:31:32 +0300

    Update register.js

--- api/server/register.js
+++ api/server/register.js
@@ -1,4 +1,6 @@
 import { read, write } from "../apis/db.js"
+import { chatsIndex, usersIndex } from "../help/data.js"
+import { User, Result } from "../help/class.js"
 
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -7,13 +9,19 @@ export default async (req, res) => {
 
   const { login, password } = req.body
 
-  const response = await fetch(URL)
+  let indexUsers = JSON.parse(await read(usersIndex))
+  if (Object.keys(indexUsers).includes(login)) {
+    res.status(403).json({ ok: false, error: "account is found" })
+  }
+  const allowedSymbols = /^[a-z0-9_-]+$/
+  if (!allowedSymbols.test(login)) {
+    res.status(400).json({ ok: false, error: "Unsupported symbols", "supported": "a-z, A-Z, 0-9, _, -" })
+  }
+  if (login.length < 3 || login.length > 23) {
+    res.status(400).json({ ok: false, error: "account length limit" })
+  }
+  const user = new User(login, password, Object.keys(indexUsers).length + 2)
+  indexUsers[login] = user.JSON
 
-  const arrayBuffer = await response.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-
-  const contentType = response.headers.get('content-type') || 'image/jpeg';
-  res.setHeader('Content-Type', contentType);
-
-  res.status(200).send(buffer);
+  res.status(200).json(new Result(true, { id: user.id }));
 }
