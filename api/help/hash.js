f7adebe54b20e6b5f4a62ca7101d4c886eb2941a
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-12 23:14:51 +0300

    Update hash.js

--- api/help/hash.js
+++ api/help/hash.js
@@ -1,3 +1,17 @@
-const genHash = (meta) => {}
+import { DB_URL, DB_SECRET } from "../help/env.js"
+
+const genHash = async (meta) => {
+  const hash = await (await fetch(`${DB_URL}/genHash`, {
+    method: "GET",
+    body: JSON.stringtify({
+      org: meta.org,
+      user: meta.user,
+      date: meta.date,
+      active: meta.active,
+      DB_SECRET
+    })
+  })).json()
+  return hash.result
+}
 
 export { genHash }
