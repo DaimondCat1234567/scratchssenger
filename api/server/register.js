e5f86ababdd711327a12b73669b9f0d76dc53b40
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-12 21:52:26 +0300

    Update register.js

--- api/server/register.js
+++ api/server/register.js
@@ -1,1 +1,19 @@
+import { read, write } from "../apis/db.js"
 
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  const { login, password } = req.body
+
+  const response = await fetch(URL)
+
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  const contentType = response.headers.get('content-type') || 'image/jpeg';
+  res.setHeader('Content-Type', contentType);
+
+  res.status(200).send(buffer);
+}
