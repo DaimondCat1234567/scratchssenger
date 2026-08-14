d15af8dc32132018ac46f1eba68a7f59d447e705
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 18:02:14 +0300

    Update main.js

--- js/web-client/main.js
+++ js/web-client/main.js
@@ -1,19 +1,19 @@
 import { HtmlElement, root, HtmlContent } from "./render.js"
 
 const leftBar = new HtmlContent([
-  new HtmlElement("div", { id: "messengerName", class: "messengerNameBar" }, "Scratchssenger"),
-  new HtmlElement("div", { id: "chatsBar", class: "chatsBar" }, "No chats!")
+  new HtmlElement("div", { id: "messengerName", class: "messengerNameBar" }, "Scratchssenger").render(),
+  new HtmlElement("div", { id: "chatsBar", class: "chatsBar" }, "No chats!").render()
 ], "div")
 const rightBar = new HtmlContent([
-  new HtmlElement("div", { id: "chatName", class: "chatNameBar" }, "Loading..."),
-  new HtmlElement("div", { id: "messages", class: "messages" }, "Loading..."),
-  new HtmlElement("div", { id: "messageInput", class: "messageInput" }, "Loading...")
+  new HtmlElement("div", { id: "chatName", class: "chatNameBar" }, "Loading...").render(),
+  new HtmlElement("div", { id: "messages", class: "messages" }, "Loading...").render(),
+  new HtmlElement("div", { id: "messageInput", class: "messageInput" }, "Loading...").render()
 ], "div")
 leftBar.render()
 rightBar.render()
 const bodyContent = new HtmlContent([
-  new HtmlElement("div", { id: "leftBar", class: "leftBar" }, String(leftBar)),
-  new HtmlElement("div", { id: "rightBar", class: "rightBar" }, String(rightBar))
+  new HtmlElement("div", { id: "leftBar", class: "leftBar" }, String(leftBar)).render(),
+  new HtmlElement("div", { id: "rightBar", class: "rightBar" }, String(rightBar)).render()
 ], "div", "root")
 bodyContent.render()
 
