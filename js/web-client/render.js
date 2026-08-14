37eee613d8aa9985389a6c43ba2f002ce06db76c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 21:22:54 +0300

    Update render.js

--- js/web-client/render.js
+++ js/web-client/render.js
@@ -8,7 +8,7 @@ class HtmlElement {
   }
   render () {
     for (let i = 0; i < this.paramsKeys.length; i++) {
-      if (this.paramsKeys[i] !== "onclick" && this.paramsKeys[i] !== "class" && this.paramsKeys[i] !== "id" && this.paramsKeys[i] !== "for") {
+      if (this.paramsKeys[i] !== "onclick" && this.paramsKeys[i] !== "class" && this.paramsKeys[i] !== "for") {
         this.element[this.paramsKeys[i]] = this.params[this.paramsKeys[i]]
       } else {
         this.element.setAttribute(this.paramsKeys[i], this.params[this.paramsKeys[i]])
