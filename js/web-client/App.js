f2a2b35889abf49283ffaf6af99fd291001c3d5c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 20:04:22 +0300

    Update App.js

--- js/web-client/App.js
+++ js/web-client/App.js
@@ -2,12 +2,6 @@ import { HtmlElement, HtmlContent } from "./render.js"
 import { AppName } from "../metadata.js"
 
 const createApp = async (loading, screen) => {
-  let chatsBar = loading
-  let chatName = loading
-  let chatMembers = loading
-  let messages = loading
-  let messageInput = loading
-  let [chatNameBar, leftBar, rightBar, bodyContent] = ""
   setTimeout(() => {
     chatNameBar = new HtmlContent([
       new HtmlElement("h1", {}, chatName).render(),
