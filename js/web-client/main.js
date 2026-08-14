0d13ad5f958499ed3cdec4ae147236f762fcbc51
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 17:36:56 +0300

    Update main.js

--- js/web-client/main.js
+++ js/web-client/main.js
@@ -1,3 +1,13 @@
 import { HtmlElement, root, HtmlContent } from "./render.js"
 
-root.innerHTML = new HtmlContent()
+const chatsBar = new HtmlContent()
+const chatBar = new HtmlContent()
+chatsBar.render()
+charBar.render()
+const bodyContent = new HtmlContent([
+  new HtmlElement("div", { id: "chats", class: "chatsBar", String(chatsBar) }),
+  new HtmlElement("div", { id: "chatBar", class: "chatBar", String(chatBar) })
+], "div", "root")
+bodyContent.render()
+
+root.innerHTML = String(bodyContent)
