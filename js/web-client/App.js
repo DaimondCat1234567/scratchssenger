d491ef75315c0edebd8b086e6a49b3aac206832b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 19:54:12 +0300

    Update App.js

--- js/web-client/App.js
+++ js/web-client/App.js
@@ -1,6 +1,35 @@
-import { HtmlElement, root, HtmlContent } from "./render.js"
+import { HtmlElement, HtmlContent } from "./render.js"
 import { AppName } from "../metadata.js"
 
-const createApp = (loading, screen) => {}
+const createApp = async (loading, screen) => {
+  let chatsBar = loading
+  let chatName = loading
+  let chatMembers = loading
+  let chatNameBar = new HtmlContent([
+    new HtmlElement("h1", {}, chatName).render(),
+    new HtmlElement("span", {}, chatMembers).render()
+  ], "div")
+  chatNameBar.render()
+  let messages = Load
+  let messageInput = Load
+  let leftBar = new HtmlContent([
+    new HtmlElement("div", { id: "messengerName", className: "messengerNameBar" }, AppName).render(),
+    new HtmlElement("div", { id: "chatsBar", className: "chatsBar" }, chatsBar).render()
+  ], "div")
+  let rightBar = new HtmlContent([
+    new HtmlElement("div", { id: "chatName", className: "chatNameBar" }, chatNameBar).render(),
+    new HtmlElement("div", { id: "messages", className: "messages" }, messages).render(),
+    new HtmlElement("div", { id: "messageInput", className: "messageInput" }, messageInput).render()
+  ], "div")
+  leftBar.render()
+  rightBar.render()
+  let bodyContent = new HtmlContent([
+    new HtmlElement("div", { id: "leftBar", className: "leftBar" }, String(leftBar)).render(),
+    new HtmlElement("div", { id: "rightBar", className: "rightBar" }, String(rightBar)).render()
+  ], "div")
+  bodyContent.render()
+
+  screen.innerHTML = bodyContent
+}
 
 export default createApp
