b84ddd5cdbb37e598aff51e5f6a7ddb3f1366bc3
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 18:38:33 +0300

    Update main.js

--- js/web-client/main.js
+++ js/web-client/main.js
@@ -1,19 +1,19 @@
 import { HtmlElement, root, HtmlContent } from "./render.js"
 
 const leftBar = new HtmlContent([
-  new HtmlElement("div", { id: "messengerName", class: "messengerNameBar", unkn: "ouwn" }, "Scratchssenger").render(),
-  new HtmlElement("div", { id: "chatsBar", class: "chatsBar", unkn: "ouwn" }, "No chats!").render()
+  new HtmlElement("div", { id: "messengerName", class: "messengerNameBar" }, "Scratchssenger").render(),
+  new HtmlElement("div", { id: "chatsBar", class: "chatsBar" }, "No chats!").render()
 ], "div")
 const rightBar = new HtmlContent([
-  new HtmlElement("div", { id: "chatName", class: "chatNameBar", unkn: "ouwn" }, "Loading...").render(),
-  new HtmlElement("div", { id: "messages", class: "messages", unkn: "ouwn" }, "Loading...").render(),
-  new HtmlElement("div", { id: "messageInput", class: "messageInput", unkn: "ouwn" }, "Loading...").render()
+  new HtmlElement("div", { id: "chatName", class: "chatNameBar" }, "Loading...").render(),
+  new HtmlElement("div", { id: "messages", class: "messages" }, "Loading...").render(),
+  new HtmlElement("div", { id: "messageInput", class: "messageInput" }, "Loading...").render()
 ], "div")
 leftBar.render()
 rightBar.render()
 const bodyContent = new HtmlContent([
-  new HtmlElement("div", { id: "leftBar", class: "leftBar", unkn: "ouwn" }, String(leftBar)).render(),
-  new HtmlElement("div", { id: "rightBar", class: "rightBar", unkn: "ouwn" }, String(rightBar)).render()
+  new HtmlElement("div", { id: "leftBar", class: "leftBar" }, String(leftBar)).render(),
+  new HtmlElement("div", { id: "rightBar", class: "rightBar" }, String(rightBar)).render()
 ], "div", "root", "app")
 bodyContent.render()
 
