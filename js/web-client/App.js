383ad9469516001e5930d31224135fe31d36e33d
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 20:01:02 +0300

    Update App.js

--- js/web-client/App.js
+++ js/web-client/App.js
@@ -7,24 +7,25 @@ const createApp = async (loading, screen) => {
   let chatMembers = loading
   let messages = loading
   let messageInput = loading
+  let [chatNameBar, leftBar, rightBar, bodyContent] = ""
   setTimeout(() => {
-    let chatNameBar = new HtmlContent([
+    chatNameBar = new HtmlContent([
       new HtmlElement("h1", {}, chatName).render(),
       new HtmlElement("span", {}, chatMembers).render()
     ], "div")
     chatNameBar.render()
-    let leftBar = new HtmlContent([
+    leftBar = new HtmlContent([
       new HtmlElement("div", { id: "messengerName", className: "messengerNameBar" }, AppName).render(),
       new HtmlElement("div", { id: "chatsBar", className: "chatsBar" }, chatsBar).render()
     ], "div")
-    let rightBar = new HtmlContent([
+    rightBar = new HtmlContent([
       new HtmlElement("div", { id: "chatName", className: "chatNameBar" }, chatNameBar).render(),
       new HtmlElement("div", { id: "messages", className: "messages" }, messages).render(),
       new HtmlElement("div", { id: "messageInput", className: "messageInput" }, messageInput).render()
     ], "div")
     leftBar.render()
     rightBar.render()
-    let bodyContent = new HtmlContent([
+    bodyContent = new HtmlContent([
       new HtmlElement("div", { id: "leftBar", className: "leftBar" }, String(leftBar)).render(),
       new HtmlElement("div", { id: "rightBar", className: "rightBar" }, String(rightBar)).render()
     ], "div")
