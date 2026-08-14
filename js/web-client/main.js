83ab90f6274dcab369a210c2235a804f05b0daf3
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-08-14 17:59:53 +0300

    Update main.js

--- js/web-client/main.js
+++ js/web-client/main.js
@@ -1,12 +1,19 @@
 import { HtmlElement, root, HtmlContent } from "./render.js"
 
-const chatsBar = new HtmlContent()
-const chatBar = new HtmlContent()
-chatsBar.render()
-charBar.render()
+const leftBar = new HtmlContent([
+  new HtmlElement("div", { id: "messengerName", class: "messengerNameBar" }, "Scratchssenger"),
+  new HtmlElement("div", { id: "chatsBar", class: "chatsBar" }, "No chats!")
+], "div")
+const rightBar = new HtmlContent([
+  new HtmlElement("div", { id: "chatName", class: "chatNameBar" }, "Loading..."),
+  new HtmlElement("div", { id: "messages", class: "messages" }, "Loading..."),
+  new HtmlElement("div", { id: "messageInput", class: "messageInput" }, "Loading...")
+], "div")
+leftBar.render()
+rightBar.render()
 const bodyContent = new HtmlContent([
-  new HtmlElement("div", { id: "chats", class: "chatsBar" }, String(chatsBar)),
-  new HtmlElement("div", { id: "chatBar", class: "chatBar" }, String(chatsBar))
+  new HtmlElement("div", { id: "leftBar", class: "leftBar" }, String(leftBar)),
+  new HtmlElement("div", { id: "rightBar", class: "rightBar" }, String(rightBar))
 ], "div", "root")
 bodyContent.render()
 
